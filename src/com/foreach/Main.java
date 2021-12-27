40c266e33924b6c0325aa4768534c1409dc0c057
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2021-12-27 18:52:04 +0530

    code enhanced for the linear search in array

--- src/com/foreach/Main.java
+++ src/com/foreach/Main.java
@@ -6,6 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter "+n+" array elements");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println("enter an element to search in the array");
+        int item = in.nextInt();
+        System.out.println(item+" is found at: "+linearSearch(arr,item)+" position");
 
 
     }
@@ -15,7 +26,7 @@ public class Main {
     static int linearSearch(int[] array, int item){
         for (int i = 0; i < array.length; i++) {
             if(item==array[i]){
-                return array[i];
+                return i+1;
             }
         }
         return -1;
